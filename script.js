77ba3e5045c1477a4624cc9ed8911d5e53bccec4
magdalena65 <141029202+magdalena65@users.noreply.github.com>
2023-09-12 16:01:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 <p>ID: ${element.id}</p>`;
             })
         })
-        const newComment = {
-            name: 'Agregar comentario nuevo usando el método Post',
-            body: 'El cuerpo del comentario nuevo iría aquí',
-        };
-    
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(newComment),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
-    })
 
-    .then(response => response.json())
-    .then(comment => {
-        document.getElementById('info').innerHTML += `<h1>${comment.name}</h1><p>${comment.body}</p><p>ID: ${comment.id}</p>`;
-    })
 })
+                          
